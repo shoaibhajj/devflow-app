a29f65d112ee38a4f646dc14d58b6e708ae6631a
shoaibhajj09 <shoaibhajj09@gmail.com>
2026-01-05 17:05:07 +0300

    Implement Logout and Show User Info on Home page

---
+++ components/UserAvatar.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+import ROUTES from "@/constants/routes";
+
+import { Avatar, AvatarFallback } from "./ui/avatar";
+interface Props {
+  id: string;
+  name?: string | null;
+  imageURL?: string | null;
+  className?: string;
+}
+function UserAvatar({
+  id,
+  name,
+  imageURL,
+  className = "h-9 w-9",
+}: Props) {
+  const initials = name
+    ?.split(" ")
+    .map((word) => word[0])
+    .join("")
+    .toUpperCase()
+    .slice(0, 2);
+
+  return (
+    <Link href={ROUTES.PROFILE(id)}>
+      <Avatar className={className}>
+        {imageURL ? (
+          <Image
+            src={imageURL}
+            alt={name || ""}
+            width={36}
+            height={36}
+            className="object-cover rounded-full"
+            quality={100}
+          />
+        ) : (
+          <AvatarFallback className="primary-gradient font-space-grotesk font-bold tracking-wider text-white">
+            {initials || "U"}
+          </AvatarFallback>
+        )}
+      </Avatar>
+    </Link>
+  );
+}
+
+export default UserAvatar;
--- components/navigation/navbar/MobileNavigation.tsx
+++ components/navigation/navbar/MobileNavigation.tsx
@@ -67,10 +67,10 @@ const MobileNavigation = async () => {
                 >
                   <Button
                     type="submit"
-                    className="base-medium w-fit !bg-transparent px-4 py-3"
+                    className="base-medium w-fit bg-transparent! px-4 py-3 mt-20  cursor-pointer rounded-lg border border-dark300_light900 hover:bg-dark50_light800 shadow-none"
                   >
                     <LogOut className="size-5 text-black dark:text-white" />
-                    <span className="text-dark300_light900">Logout</span>
+                    <span className="text-dark300_light900 cursor-pointer" >Logout</span>
                   </Button>
                 </form>
               </SheetClose>
@@ -78,7 +78,7 @@ const MobileNavigation = async () => {
               <>
                 <SheetClose asChild>
                   <Link href={ROUTES.SIGN_IN}>
-                    <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                    <Button className="small-medium btn-secondary min-h-10.25 w-full rounded-lg px-4 py-3 shadow-none">
                       <span className="primary-text-gradient">Log In</span>
                     </Button>
                   </Link>
@@ -86,7 +86,7 @@ const MobileNavigation = async () => {
 
                 <SheetClose asChild>
                   <Link href={ROUTES.SIGN_UP}>
-                    <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+                    <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-10.25 w-full rounded-lg border px-4 py-3 shadow-none">
                       Sign Up
                     </Button>
                   </Link>
